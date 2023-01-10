5827dfb28fce564a52dae28a553200bdabb409f0
giaquy-Dev <120464606+giaquy-Dev@users.noreply.github.com>
2023-01-10 15:48:47 +0700

    Update haru.py

--- haru.py
+++ haru.py
@@ -125,7 +125,7 @@ _\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'_
     clear()
 
 def si():
-    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233m1337 \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to HaRu attack CnC! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mGithub: giaquy-Dev \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233m25 Methods Attack!|')
+    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mHaRu \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to HaRu attack CnC! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233m25 Methods Attack!|')
     print("")
 
 def tools():
@@ -223,7 +223,7 @@ def amp_games():
 def menu():
     sys.stdout.write(f"\x1b]2;HaRu PNL --> Stars: [{bots}] | Online Users: [1] | Methods: [25] | Bypass: [10] | Amps: [1]\x07")
     clear()
-    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233m1337 \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to HaRu attack CnC! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mGithub: giaquy-dev \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233m25 Methods Attack!')
+    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mHaRu \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to HaRu attack CnC! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233m25 Methods Attack!')
     print("")
     print("""
                 \x1b[38;2;0;212;14m╔═══════════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════════╗
